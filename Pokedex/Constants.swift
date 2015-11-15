59150a01e89f85e883d6ac8e336edeb9e900309f
patricklargen <patricklargen@gmail.com>
2015-11-14 19:14:34 -0600

    installed AlamoFire

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Patrick Largen on 11/14/15.
+//  Copyright © 2015 Patrick Largen. All rights reserved.
+//
+
+import Foundation
+
+//These are globally accessable since they aren't in a class
+
+let BASE_URL = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/"
+
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -9,19 +9,34 @@
 import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    
+    
     var pokemon: Pokemon!
     
+    @IBOutlet weak var pokemonNameLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var baseAtack: UILabel!
+    @IBOutlet weak var currentEvoImage: UIImageView!
+    @IBOutlet weak var nextEvoImage: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("I GOT HERE")
         nameLabel.text = pokemon.name
         
-       
+        
     }
-
-
-
+    
+    @IBAction func backButtonPressed(sender: UIButton) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
